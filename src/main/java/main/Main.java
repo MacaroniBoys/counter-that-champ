2744b3dc4297e282f1e8d98fa069f8a5ab7fab50
Cary Ciccone <Cary@bricksimple.com>
2018-02-17 15:39:56 -0500

    added a simple view, fixed some maven issues.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,4 +1,4 @@
-package sample;
+package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -10,8 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
